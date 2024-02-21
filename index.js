52b126ce00a07669898c2266fe19065c974e3c07
Log_4n <joakinpalacios123gmail.com>
2024-02-21 16:45:03 -0300

    finished

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+let texto = document.querySelector('.jorge');
 
+texto.addEventListener('mouseenter',()=>{
+    texto.textContent = 'Logan';
+    texto.classList.add('color-changing');
+});
+
+texto.addEventListener('mouseleave',()=>{
+    texto.textContent = 'Jorge';
+    texto.classList.remove('color-changing');
+});
+
+// var i = 0;
+// var txt = 'console.log(“El misterio de la vida no es problema que hay que resolver, sino un a realidad que hay que experimentar.”);'; /* The text */
+// var speed = 50;
+
+// function typeWriter() {
+//   if (i < txt.length) {
+//     document.querySelector(".typing-animation").innerHTML += txt.charAt(i)+=;
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+// }
+
+// typeWriter();
